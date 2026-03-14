b3aec9b44e8ad230d6eaa42b7de7f1cd0a0624ba
haliphax <haliphax@gmail.com>
2026-03-14 06:08:53 +0000

    fix: allow draft posts to build for preview

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,6 +1,5 @@
 module.exports = {
   eleventyComputed: {
     eleventyExcludeFromCollections: (data) => data.draft ? true : undefined,
-    permalink: (data) => data.draft ? false : undefined,
   }
 };
